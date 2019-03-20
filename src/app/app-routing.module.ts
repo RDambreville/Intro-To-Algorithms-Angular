14bd7e33920ff34f98ecf4aa9fc7822742390661
bobDambreville <bob.dambreville@prutech.com>
2019-03-19 23:14:37 -0400

    Created Insertion Sort

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { InsertionSortComponent } from './sorting/insertion-sort/insertion-sort.component';
 import { RadixSortComponent } from './sorting/radix-sort/radix-sort.component';
 import { CountingSortComponent } from './sorting/counting-sort/counting-sort.component';
 import { HeapSortComponent } from './sorting/heap-sort/heap-sort.component';
@@ -6,6 +7,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {path: 'insertion-sort', component: InsertionSortComponent},
   {path: 'quick-sort', component: QuickSortComponent},
   {path: 'heap-sort', component: HeapSortComponent},
   {path: 'counting-sort', component: CountingSortComponent},
--- src/app/sorting/insertion-sort/insertion-sort.component.ts
+++ src/app/sorting/insertion-sort/insertion-sort.component.ts
@@ -1,3 +1,4 @@
+import { SortingService } from './../../shared/services/sorting.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsertionSortComponent implements OnInit {
 
-  constructor() { }
+  A: any[] = []; // input array
+  B: any[] = []; // sorted output array
+
+  constructor(
+    private sortService: SortingService
+  ) { }
 
   ngOnInit() {
+  }
+
+  insertionSort() {
+    this.B = this.A;
+    const arrLen = this.B.length;
+    let key;
+    for (let j = 1; j < arrLen; j++) {
+      key = this.B[j];
+      let i = j - 1;
+      while (i >= 0 && this.B[i] > key) {
+        this.B[i + 1] = this.B[i];
+        i--;
+      }
+      this.B[i + 1] = key;
+    }
+    console.log('sorted array', this.A);
   }
 
 }
