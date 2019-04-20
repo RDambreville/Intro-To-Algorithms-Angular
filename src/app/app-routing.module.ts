611dda13aa4881f2321e3d77e787daff9aefe43a
bobDambreville <bob.dambreville@prutech.com>
2019-04-20 15:40:53 -0400

    Added basic merge sort template and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { InsertionSortComponent } from './sorting/insertion-sort/insertion-sort.component';
+import { MergeSortComponent } from './sorting/merge-sort/merge-sort.component';
 import { RadixSortComponent } from './sorting/radix-sort/radix-sort.component';
 import { CountingSortComponent } from './sorting/counting-sort/counting-sort.component';
 import { HeapSortComponent } from './sorting/heap-sort/heap-sort.component';
@@ -8,6 +9,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path: 'insertion-sort', component: InsertionSortComponent},
+  {path: 'Merge-sort', component: MergeSortComponent},
   {path: 'quick-sort', component: QuickSortComponent},
   {path: 'heap-sort', component: HeapSortComponent},
   {path: 'counting-sort', component: CountingSortComponent},
